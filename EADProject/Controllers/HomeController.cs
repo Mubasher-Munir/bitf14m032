232667f90bbd406f5a54f30c1eb7b7bb2ef34380
Mubasher-Munir <sheikh.mubasher.munir@gmail.com>
2017-07-10 22:38:50 +0500

    Furniture Project

---
+++ EADProject/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using EADProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EADProject.Controllers
+{
+
+      // [Authorize (Roles= "Admin , User"  )]
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            FurnitureDBEntities1 obj = new FurnitureDBEntities1();
+
+            //var productsList = ctx.Products.ToList();
+            //  if (productsList == null)
+            // {
+            //productsList = new List<Product>();
+            // }
+            return View(obj.Galleries.ToList());
+        }
+        public ActionResult Services()
+        {
+            FurnitureDBEntities1 obj = new FurnitureDBEntities1();
+            //var productsList = ctx.Products.ToList();
+            //  if (productsList == null)
+            // {
+            //productsList = new List<Product>();
+            // }
+            return View(obj.Products.ToList());
+        }
+    }
+}
---
+++ EADProject/Startup.cs
@@ -0,0 +1,66 @@
+﻿using EADProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EADProject.Startup))]
+namespace EADProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser();
+                user.UserName = "mubasher";
+                user.Email = "mubi@gmail.com";
+
+                string userPWD = "Mubi@123";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
